41790c8a4ae00073646f64f8ec373768dba2286d
Dario Jakovljević <jakovljevic.dario@gmail.com>
2022-03-24 13:03:44 +0100

    login -works 2

--- banking/Main.java
+++ banking/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isMainMenu = true;
+        boolean isSubMenu = true;
         int mainChoice;
         
         do {
+            // main menu
             System.out.println(
                     "1. Create an account\n" +
                     "2. Log into account\n" +
@@ -16,7 +18,6 @@ public class Main {
                     "0. Exit");
             mainChoice = scanner.nextInt();
 
-            // main menu
             switch (mainChoice) {
                 // create account
                 case 1:
@@ -26,39 +27,53 @@ public class Main {
                 case 2:
                     if (AccountManager.loginToAcc()) {
                         System.out.println("\nYou have successfully logged in!\n");
-                        System.out.println("1. Balance\n" +
-                                "2. Log out\n" +
-                                "0. Exit");
-                        int subChoice = scanner.nextInt();
-                        scanner.nextLine();
 
-                        // submenu
-                        switch (subChoice) {
-                            case 1:
-                                System.out.println("\nBalance: " + AccountManager.getAccBalance());
-                                break;
-                            case 2:
-                                break;
-                            case 3:
-                                isMainMenu = false;
-                                break;
-                            default:
-                                System.out.println("Wrong input");
-                        }
+                        do {
+                            // submenu
+                            System.out.println("User options: ");
+                            System.out.println(
+                                    "1. Balance\n" +
+                                    "2. Log out\n" +
+                                    "0. Exit");
+                            int subChoice = scanner.nextInt();
+                            scanner.nextLine();
+
+                            switch (subChoice) {
+                                // get bank balance for the user
+                                case 1:
+                                    System.out.println("\nBalance: " + AccountManager.getAccBalance());
+                                    break;
+                                // logout
+                                case 2:
+                                    isSubMenu = false;
+                                    break;
+                                // exit
+                                case 0:
+                                    isSubMenu = false;
+                                    isMainMenu = false;
+                                    System.out.println("exiting...");
+                                    break;
+                                // for wrong input
+                                default:
+                                    System.out.println("Wrong input");
+                            }
+
+                        } while (isSubMenu);
 
                     } else {
                         System.out.println("Wrong credentials");
                     }
                     break;
-
+                // number of available acc slots
                 case 5:
                     System.out.println("\nNumber of available Acc slots: " + AccountManager.getNumOfFreeSlots() + "\n");
                     break;
-
+                // exit program
                 case 0:
                     isMainMenu = false;
                     System.out.println("exiting...");
                     break;
+                // for wrong input
                 default:
                     System.out.println("Wrong input");
             }
